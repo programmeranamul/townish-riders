d5467ef1ee4740e971f0072c6634ee70050d2b03
programmeranamul <programmer.anamul@gmail.com>
2021-03-21 07:06:47 -0700

    solve

--- src/Compoent/Contact/Contact.js
+++ src/Compoent/Contact/Contact.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="mt-3 container border-top pt-4">
-            <h2>This Is Contact Page....</h2>            
+            <h2>jkdgh.</h2>            
         </div> 
     );
 };
