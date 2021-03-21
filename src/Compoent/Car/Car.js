a6077edcbb8b85bb2e1da674a6ae029c87e456df
programmeranamul <programmer.anamul@gmail.com>
2021-03-21 06:49:21 -0700

    commite 3

--- src/Compoent/Car/Car.js
+++ src/Compoent/Car/Car.js
@@ -6,7 +6,7 @@ const Car = (props) => {
     const history = useHistory();
     const { typeOfRides, rideImage } = props.car
     return (
-        <article className="col-md-3 text-center" onClick={() => history.push(`/destination/${typeOfRides}`)}>
+        <article className="col-md-3 text-center mt-5 mt-md-0" onClick={() => history.push(`/destination/${typeOfRides}`)}>
             <div className="single-ride">
                 <img src={rideImage} alt={typeOfRides} className="w-50" />
                 <h4 className="mt-3">{typeOfRides}</h4>
